c6106fa96c7f1cf6499dc1f7055216e568563fa7
カズニン <paraysis@gmail.com>
2020-09-28 10:41:58 +0900

    Manipulation
    Commit is fadeOutLogic.

--- BallLdyProject/Assets/Scripts/PanelScript.cs
+++ BallLdyProject/Assets/Scripts/PanelScript.cs
@@ -6,6 +6,8 @@ public class PanelScript : MonoBehaviour {
     //Material m_Material;
     [SerializeField] private CanvasGroup a;
     float clearLevel = 1.0f;
+    // [visibled / none]
+    string displayStates;
     bool fadeOutSwitch = false;
 
     // Use this for initialization
@@ -13,8 +15,9 @@ public class PanelScript : MonoBehaviour {
         //m_Material = GetComponent<Renderer>().material;
         //m_Material.color = Color.red;
         //m_Material.color = new Color(0, 0, 0, 1.0f);
-        
+
         //Debug.Log("動く？");
+        displayStates = "visibled";
     }
 	
 	// Update is called once per frame
@@ -28,17 +31,40 @@ public class PanelScript : MonoBehaviour {
             fadeOutSwitch = true;
         }
 
-        if (fadeOutSwitch && 0f <= clearLevel)
+        if (displayStates.Equals("visibled") && fadeOutSwitch && 0f <= clearLevel)
         {
-            clearLevel = clearLevel - 0.1f;
+            //StartCoroutine("Corou3");
+            Debug.Log("clearLevel_bef：[" + clearLevel + "]");
+            clearLevel -= 0.1f;
             a.alpha = clearLevel;
+            Debug.Log("clearLevel_aft：[" + clearLevel + "]");
+        }
+        else if (fadeOutSwitch)
+        {
+            fadeOutSwitch = false;
+            clearLevel = 0f;
+            displayStates = "none";
             Debug.Log("clearLevel：[" + clearLevel + "]");
+            //Debug.Log("clearLevel：[" + clearLevel + "]");
+            //Debug.Log("0になった");
         }
         else
         {
-            fadeOutSwitch = false;
+            Debug.Log("それ以外");
             Debug.Log("clearLevel：[" + clearLevel + "]");
-            Debug.Log("0になった");
         }
+    }
+    //コルーチン関数を定義
+    private IEnumerator Corou3() //コルーチン関数の名前
+    {
+        yield return new WaitForSeconds(1.0f);
+        clearLevel -= 0.1f;
+        a.alpha = clearLevel;
+        Debug.Log("clearLevel：[" + clearLevel + "]");
+
+        //コルーチンの内容
+        //Debug.Log("a");
+        //yield return null;
+        //Debug.Log("b");
     }
 }
