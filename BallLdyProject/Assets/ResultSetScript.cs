4c759269620e58f32a7d0675fc0e1feaad35a5f8
カズニン <paraysis@gmail.com>
2020-09-30 11:08:01 +0900

    GoalFlag
    Update GoalEventScript.cs
      TimerScore throw is BallLdyResult_01.
    Create Scene BallLdyResult_01.
    Create GameObject ResultSetter.
    Insert ResultSetScript.cs
      Getter TimerScore.

---
+++ BallLdyProject/Assets/ResultSetScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultSetScript : MonoBehaviour {
+    public float resultTime;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("シーン遷移済み");
+        Debug.Log(resultTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
